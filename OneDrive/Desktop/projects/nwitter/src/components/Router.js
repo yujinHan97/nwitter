5f79c28d04ff2bcfe526c0dd1cac8d278113ec2c
yujinHan97 <dbwlslek97@gmail.com>
2022-01-09 13:54:31 +0900

    3.0 Sign up, Sign in(+ social log-in)

--- OneDrive/Desktop/projects/nwitter/src/components/Router.js
+++ OneDrive/Desktop/projects/nwitter/src/components/Router.js
@@ -1,22 +1,29 @@
-import { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
-                    <Route exact path = "/">
-                        <Home />
-                    </Route>
+                    <>
+                        <Route exact path = "/">
+                            <Home />
+                        </Route>
+                        <Route exact path = "/profile">
+                            <Profile />
+                        </Route>
+                    </>
                 ) : (
                     <Route exact path = "/">
                         <Auth />
                     </Route>
                 )}
+                {/* <Redirect from="*" to="/" /> */}
             </Switch>
         </Router>
     );
